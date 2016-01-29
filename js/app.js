1ef5ec84a3f81755edaff5ab7daf252a35c47aa1
dawnvits <dev.dawn024@gmail.com>
2016-01-29 12:43:09 +0800

    Finished

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
+// Reset player to beginning position
+Object.prototype.reset = function() {
+  player.x = 200;
+  player.y = 400;
+}
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    // x and y coordinates and movement speed
+    this.x = x;
+    this.y = y;
+    this.speed = Math.floor((Math.random() * 200) + 100);
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +25,20 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    
+    //if the enemy crosses off screen, reset its position. Otherwise, it keeps running.
+    if(this.x <= 550){
+        this.x += this.speed * dt;
+    }else{
+        this.x = -2;
+    }
+
+    //If the player comes within 30px of an enemy's x and y coordinates, reset the game
+    if(player.x >= this.x - 30 && player.x <= this.x + 30){
+        if(player.y >= this.y - 30 && player.y <= this.y + 30){
+            this.reset();
+        }
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +49,54 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function () {
+    this.sprite = 'images/char-boy.png';
+    this.x = 200;
+    this.y = 400;
+};
 
+Player.prototype.update = function(){
+    //if left key is pressed and player is not on edge of map, pressed decrement x
+    if(this.ctlKey === 'left' && this.x > 0){ 
+        this.x = this.x - 50;
+    //if right key is pressed and player is not on edge of map increment x 
+    }else if(this.ctlKey === 'right' && this.x != 400){
+        this.x = this.x + 50;
+    //if up key is pressed increment y 
+    }else if(this.ctlKey === 'up'){
+        this.y = this.y - 50;
+    //if down key is pressed and player is not on edge of map decrement y 
+    }else if (this.ctlKey === 'down' && this.y != 400){
+        this.y = this.y + 50;
+    }
+    this.ctlKey = null;
+    
+    //If on water, reset
+    if(this.y < 25){
+        this.reset();
+    }
+}
+
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+//Input handler for player
+Player.prototype.handleInput = function(e){
+    this.ctlKey = e;    
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+// // Instantiate enemies and player objects
+var enemy1 = new Enemy(-2, 60);
+var enemy2 = new Enemy(-2, 100);
+var enemy3 = new Enemy(-2, 150);
+var enemy4 = new Enemy(-2, 220);
 
-
+var allEnemies = [enemy1, enemy2, enemy3, enemy4];
+var player = new Player(); 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
